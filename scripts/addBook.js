4deffea50e601760a66d2bfd7dc191f05eb58022
armin-164 <arminsadr164@hotmail.com>
2024-01-22 17:22:48 +0100

    Add function that will post form values to the library

---
+++ scripts/addBook.js
@@ -0,0 +1,31 @@
+function addBookData() {
+    const inputTitle = document.getElementById('title').value;
+    const inputPages = document.getElementById('pages').value;
+    const inputAuthor = document.getElementById('author').value;
+    const inputLoanStatus = document.getElementById('loan-status').checked;
+
+    if (inputTitle !== '' && inputPages !== '' && inputAuthor !== '') {
+        let book = {
+            author: inputAuthor,
+            pages: inputPages,
+            title: inputTitle,
+            isLoaned: inputLoanStatus
+        }
+
+        fetch('http://localhost:4444/library', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(book)
+          })
+          .then(response => response.json())
+          .then(data => console.log('Server response:', data))
+          .catch(error => console.error('Error:', error));
+        
+
+        console.log(book)
+    }
+}
+
+export {addBookData}
